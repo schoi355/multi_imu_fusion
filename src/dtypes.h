fe1d29310cd8bf3c24c9799cc3fadd678f64b8ba
jongwonjlee <jongwon5@illinois.edu>
2023-01-11 13:36:07 -0600

    initialize codebase

---
+++ src/dtypes.h
@@ -0,0 +1,17 @@
+#ifndef DTYPES_H_
+#define DTYPES_H_
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+} Axis3f;
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+  float w;
+} Quat4f;
+
+#endif /* DTYPES_H_ */
